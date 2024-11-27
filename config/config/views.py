322646a2aa16a732e4aae04c967a8f1b51a2e728
Qobiljon <madrahimovq@gmail.com>
2024-11-27 17:35:12 +0500

    2-kun

---
+++ config/config/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+
+from django.core.handlers.wsgi import WSGIRequest
+from django.http import HttpResponse
+
+
+def home(request: WSGIRequest):
+    return render(request, 'index.html')
+
+
+def about(request: WSGIRequest):
+    return HttpResponse("<h1>FN26 guruhi<h1>")
